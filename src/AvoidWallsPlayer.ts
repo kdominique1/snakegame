ce000eefcc2a5999b95571102ff90ba1ff7278f7
kdominique1 <131176593+kdominique1@users.noreply.github.com>
2024-03-05 16:09:46 +0000

    Added all possibilities to makeTurn method

--- src/AvoidWallsPlayer.ts
+++ src/AvoidWallsPlayer.ts
@@ -7,18 +7,20 @@ class AvoidWallsPlayer extends Player {
     }
 
     makeTurn() {
-        /* If the snake's direction is left, the x-coord is 0, and the y-coord is less than the total model height
+        /* If the snake's direction is left, the x-coord is 0, and the y-coord is less than half the total model height
         OR if the snake's direction is down, the y-coord is less than or equal to 0, and the x-coord is less than half the model width
         OR if the snake's direction is up and the y-coord is equal to or greater than the total model height and the x-coord is equal to or more than half of the world width
+        OR if the snake's direction is right, the x-coord is equal to or greater than the total model width, and the y-coord is less than or equal to more than half of the world height
         */
-        if ((this.sc.snakeDirection === -1 && this.sc.snakePosition.x === 0 && this.sc.snakePosition.y < this.sc.worldHeight) || (this.sc.snakeDirection === 2 && this.sc.snakePosition.y <= 0 && this.sc.snakePosition.x < this.sc.worldWidth/2) || (this.sc.snakeDirection == 0 && this.sc.snakePosition.y >= this.sc.worldHeight && this.sc.snakePosition.x >= this.sc.worldWidth/2)) {
+        if ((this.sc.snakeDirection === -1 && this.sc.snakePosition.x === 0 && this.sc.snakePosition.y < this.sc.worldHeight/2) || (this.sc.snakeDirection === 2 && this.sc.snakePosition.y <= 0 && this.sc.snakePosition.x < this.sc.worldWidth/2) || (this.sc.snakeDirection === 0 && this.sc.snakePosition.y >= this.sc.worldHeight && this.sc.snakePosition.x >= this.sc.worldWidth/2) || (this.sc.snakeDirection === 1 && this.sc.snakePosition.x >= this.sc.worldWidth && this.sc.snakePosition.y >= this.sc.worldHeight/2)) {
             this.sc.turnSnakeLeft();
         }
         /* If the snake's direction is up and the y-coord is equal to or greater than the total model height and the x-coord is less than half of the world width
         OR if the snake's direction is right and the x-coord is equal to or greater than the total model width and the y-coord is greater than 0
         OR if the snake's direction is down, the y-coord is less than or equal to 0, and the x-coord is equal to or more than half the model width
+        OR if the snake's direction is left, the x-coord is less than or equal to 0, and the y-coord is less than half the model height
         */
-        else if ((this.sc.snakeDirection === 0 && this.sc.snakePosition.y >= this.sc.worldHeight) || (this.sc.snakeDirection === 1 && this.sc.snakePosition.x >= this.sc.worldWidth && this.sc.snakePosition.y > 0) || (this.sc.snakeDirection === 2 && this.sc.snakePosition.y <= 0 && this.sc.snakePosition.x >= this.sc.worldWidth/2)) {
+        else if ((this.sc.snakeDirection === 0 && this.sc.snakePosition.y >= this.sc.worldHeight) || (this.sc.snakeDirection === 1 && this.sc.snakePosition.x >= this.sc.worldWidth && this.sc.snakePosition.y > 0) || (this.sc.snakeDirection === 2 && this.sc.snakePosition.y <= 0 && this.sc.snakePosition.x >= this.sc.worldWidth/2) || (this.sc.snakeDirection === -1 && this.sc.snakePosition.x <= 0 && this.sc.snakePosition.y < this.sc.worldHeight/2)) {
             this.sc.turnSnakeRight();
 }
     }
