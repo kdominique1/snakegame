95a9eaccfd9737cf093386d030982aec7ca63144
kdominique1 <131176593+kdominique1@users.noreply.github.com>
2024-06-05 03:13:31 +0000

    added iinputhandler and lrkeyinputhandler classes

---
+++ src/LRKeyInputHandler.ts
@@ -0,0 +1,11 @@
+class LRKeyInputHandler {
+  private wasLeftArrowPushed: Boolean;
+  private wasRightArrowPushed: Boolean;
+
+  constructor() {
+    this.wasLeftArrowPushed = false;
+    this.wasRightArrowPushed = false;
+  }
+}
+
+export default LRKeyInputHandler;
