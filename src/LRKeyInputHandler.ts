7aac095813f6243c10603b4a76f22c45f3e05f32
kdominique1 <131176593+kdominique1@users.noreply.github.com>
2024-06-05 03:34:39 +0000

    added rest of lrkeyinputhandler class

--- src/LRKeyInputHandler.ts
+++ src/LRKeyInputHandler.ts
@@ -1,10 +1,35 @@
 class LRKeyInputHandler {
-  private wasLeftArrowPushed: Boolean;
-  private wasRightArrowPushed: Boolean;
+  private wasLeftArrowPushed: boolean;
+  private wasRightArrowPushed: boolean;
 
   constructor() {
     this.wasLeftArrowPushed = false;
     this.wasRightArrowPushed = false;
+  }
+
+  // Event listener
+  private handleKeydown(event: KeyboardEvent): void {
+    if (event.key === "ArrowLeft") {
+      this.wasLeftArrowPushed = true;
+    } else if (event.key === "ArrowRight") {
+      this.wasRightArrowPushed = true;
+    }
+  }
+
+  public resetLeftMove() {
+    this.wasLeftArrowPushed = false;
+  }
+
+  public resetRightMove() {
+    this.wasRightArrowPushed = false;
+  }
+
+  public get madeLeftMove(): boolean {
+    return this.wasLeftArrowPushed;
+  }
+
+  public get madeRightMove(): boolean {
+    return this.wasRightArrowPushed;
   }
 }
 
