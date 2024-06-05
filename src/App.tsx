00d111606e775af842dbe589cc2a971df2f40bdc
kdominique1 <131176593+kdominique1@users.noreply.github.com>
2024-06-05 17:12:37 +0000

    updated app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -18,8 +18,7 @@ export default function App() {
     const greenSnake = new Snake();
     const brownSnake = new Snake();
     const redSnake = new Snake();
-    const worldModel1 = new WorldModel(redSnake);
-    const worldModel2 = new WorldModel(greenSnake);
+    const worldModel1 = new WorldModel(greenSnake);
     const iInputHandler1 = new LRKeyInputHandler();
     const canvasWorldView = new CanvasWorldView(10);
     const snakeController1 = new SnakeController(worldModel1, greenSnake);
@@ -27,16 +26,17 @@ export default function App() {
     const avoidWallsPlayer1 = new AvoidWallsPlayer(snakeController1);
     const humanPlayer1 = new HumanPlayer(snakeController1, iInputHandler1);
 
-    worldModel2.view = canvasWorldView;
-    worldModel2.updateSteps(1);
+    worldModel1.view = canvasWorldView;
+    worldModel1.updateSteps(1);
 
     greenSnake.move(2);
     greenSnake.move(2);
     brownSnake.move(1);
-    worldModel2.updateSteps(5);
-    display("Green snake's current position is: " + greenSnake.position);
+    worldModel1.updateSteps(5);
+
     display("Brown snake's current position is: " + brownSnake.position);
-    display("Red snake's current position is: " + worldModel1.snake.position);
+    display("Green snake's current position is: " + worldModel1.snake.position);
+
     gameController1.run();
   }, []);
 
