ca59627c8ce0bca681830113b2a06a855a4858d5
kdominique1 <131176593+kdominique1@users.noreply.github.com>
2024-06-05 05:40:10 +0000

    updated gamecontroller class

--- src/GameController.ts
+++ src/GameController.ts
@@ -1,4 +1,5 @@
 import WorldModel from "./WorldModel";
+import Player from "./Player";
 
 class GameController {
   private world: WorldModel;
@@ -7,11 +8,33 @@ class GameController {
     this.world = worldModel;
   }
 
-  run() {}
+  set player1(firstPlayer: Player) {
+    this.player1 = firstPlayer;
+  }
 
-  updateFrame() {}
+  set player2(secondPlayer: Player) {
+    this.player2 = secondPlayer;
+  }
 
-  requestAnimationFrame() {}
+  run() {
+    let lastTime = 0;
+  }
+
+  requestAnimationFrame() {
+    this.updateFrame();
+  }
+
+  updateFrame() {
+    this.player1.makeTurn();
+    this.player2.makeTurn();
+    let currentTime = performance.now();
+    let timeSinceLast = currentTime - lastTime;
+    if (timeSinceLast > 250) {
+      this.world.updateSteps(1);
+      lastTime += 250;
+    }
+    this.requestAnimationFrame();
+  }
 }
 
 export default GameController;
